b330ab05cc79ac3266da5af5611018e346693104
Tandy <tandy.bon@gmail.com>
2025-01-28 20:08:48 +0200

    ZVEK-3 Damage decrease update

--- src/pages/Statistic/components/DamageDecrease.tsx
+++ src/pages/Statistic/components/DamageDecrease.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, ElementType } from 'react';
+import React, {useMemo, ElementType} from 'react';
 import styled from 'styled-components';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -7,7 +7,10 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { latestZveks } from '../../../DATA';
+import {PlugCellStyles} from 'services/GlobalStyled';
+import {headerValues} from '../StatisticUtils';
+import {latestZveks} from '../../../DATA';
+import {font_header_6_bold} from 'theme/fonts';
 
 interface PlayerData {
   name: string;
@@ -17,44 +20,45 @@ interface PlayerData {
 }
 
 const DamageDecrease = () => {
-  const playerData: PlayerData[] = useMemo(() => {
-    return latestZveks
-      .map(({ name, info }) => {
-        const lastEventInfo = info[info.length - 1];
-        const previousEventInfo = info[info.length - 2] || { damage: 0 };
+  const playerData: PlayerData[] = useMemo(
+    () =>
+      latestZveks.reduce<PlayerData[]>((acc, {name, info}) => {
+        const lastTwoZveks = info.slice(-2);
+        if (lastTwoZveks.length < 2) return acc;
 
-        const damageLastZvek = lastEventInfo.damage;
-        const damagePreviousZvek = previousEventInfo.damage;
-        const percentageChange =
-          damagePreviousZvek > 0
-            ? ((damageLastZvek - damagePreviousZvek) / damagePreviousZvek) * 100
-            : null;
-        return { name, damageLastZvek, damagePreviousZvek, percentageChange };
-      })
-      .filter(({ percentageChange }) => percentageChange !== null && percentageChange < 0);
-  }, []);
+        const [previousZvek, lastZvek] = lastTwoZveks;
+        const percentageChange = ((lastZvek.damage - previousZvek.damage) / previousZvek.damage) * 100;
 
-  const headerValues = ['Никнейм', 'Урон прошлого ЗВЭК (млрд)', 'Урон последнего ЗВЭК (млрд)', 'Уменьшился на'];
+        percentageChange < 0 &&
+          acc.push({
+            name,
+            damageLastZvek: lastZvek.damage,
+            damagePreviousZvek: previousZvek.damage,
+            percentageChange,
+          });
+
+        return acc;
+      }, []),
+    []
+  );
 
   const allPlayersDamageGrow = playerData.length === 0;
 
   return (
     <Container component={Paper}>
-      <Table>
-        <TableHead>
-          { !allPlayersDamageGrow && (
-          <Row>
-            {headerValues.map((value) => (
-              <TableCell align="center" key={value}>
-                <b>{value}</b>
-              </TableCell>
-            ))}
-          </Row>
-          )}
-        </TableHead>
-        <TableBody>
-          {!allPlayersDamageGrow ? (
-            playerData.map(({ name, damagePreviousZvek, damageLastZvek, percentageChange }, idx) => {
+      {!allPlayersDamageGrow ? (
+        <Table>
+          <TableHead>
+            <Row>
+              {headerValues.map((value) => (
+                <TableCell align="center" key={value}>
+                  <b>{value}</b>
+                </TableCell>
+              ))}
+            </Row>
+          </TableHead>
+          <TableBody>
+            {playerData.map(({name, damagePreviousZvek, damageLastZvek, percentageChange}, idx) => {
               const changeText = `уменьшился на ${Math.abs(percentageChange!).toFixed(2)}%`;
 
               return (
@@ -67,21 +71,17 @@ const DamageDecrease = () => {
                   </TableCell>
                 </Row>
               );
-            })
-          ) : (
-            <NoDataRow>
-              <TableCell align="center" colSpan={4}>
-                <NoDataText>У всех игроков урон вырос</NoDataText>
-              </TableCell>
-            </NoDataRow>
-          )}
-        </TableBody>
-      </Table>
+            })}
+          </TableBody>
+        </Table>
+      ) : (
+        <Plug>У всех игроков урон вырос</Plug>
+      )}
     </Container>
   );
 };
 
-const Container = styled(TableContainer)<{ component: ElementType }>`
+const Container = styled(TableContainer)<{component: ElementType}>`
   &.MuiPaper-root {
     margin: 0 1rem;
     width: calc(100% - 2rem);
@@ -95,31 +95,24 @@ const Row = styled(TableRow)`
   }
 `;
 
-const ChangeText = styled.span<{ value: number | null }>`
+const ChangeText = styled.span<{value: number | null}>`
   color: ${({
     value,
     theme: {
-      colors: { red100, green100, gray100 },
+      colors: {red100, green100, gray100},
     },
   }) => {
-    if (value === null) return gray100;
-    if (value > 0) return green100;
-    if (value < 0) return red100;
+    if (value === null || value === 0) return gray100;
+    return value > 0 ? green100 : red100;
     return gray100;
   }};
 `;
 
-const NoDataRow = styled(TableRow)`
-  &.MuiTableRow-root {
-    display: grid;
-    grid-template-columns: repeat(1, 1fr);
-  }
-`;
-
-const NoDataText = styled.span`
-  color: ${({ theme: { colors } }) => colors.green100};
-  font-weight: bold;
-  font-size: 1.2rem;
+const Plug = styled.div`
+  ${PlugCellStyles};
+  color: ${({theme}) => theme.colors.green100};
+  text-align: center;
+  ${font_header_6_bold}
 `;
 
 export default DamageDecrease;
