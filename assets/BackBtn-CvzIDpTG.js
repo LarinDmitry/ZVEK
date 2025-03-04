be4e1d6b72904aac666e1f5ed3dc4d626291bb59
larin <Dmytro.Larin@aurelis.software>
2025-03-04 22:52:40 +0200

    Updates

---
+++ assets/BackBtn-CvzIDpTG.js
@@ -0,0 +1,22 @@
+import{w as e,x as n,y as c,F as i,j as r,v as t,A as l,S as u}from"./index-DLuJz_lx.js";import{F as g}from"./arrow-BOVO1wOE.js";import{d}from"./fonts-BC2pwPS7.js";const b=()=>{const o=e(),{language:s}=n(c),{BACK:a}=i(s);return r.jsxs(m,{onClick:()=>o("/main"),children:[r.jsx(p,{children:r.jsx(g,{})}),a]})},m=t(l)`
+  &.MuiButtonBase-root {
+    width: 5rem;
+    ${d};
+    color: ${({theme:o})=>o.colors.gray000};
+    background: ${({theme:o})=>o.colors.blue100};
+    border-radius: 16px;
+    text-transform: inherit;
+
+    &:hover {
+      background: ${({theme:o})=>o.colors.blue100};
+    }
+  }
+`,p=t(u)`
+  &.MuiSvgIcon-root {
+    width: 1rem;
+    height: 1rem;
+    cursor: pointer;
+    fill: ${({theme:o})=>o.colors.gray000};
+    transform: rotate(90deg);
+  }
+`;export{b as B};
---
+++ assets/ContactsView-DNuBNpPk.js
@@ -0,0 +1,18 @@
+import{x as E,y as x,r as u,j as e,v as o}from"./index-DLuJz_lx.js";import{B as R}from"./BackBtn-CvzIDpTG.js";import{h}from"./fonts-BC2pwPS7.js";import{P as A}from"./Paper-DBmm9sXk.js";import{a as g,b,c as l,d as j,e as D}from"./TableRow-j-kgH3iv.js";import"./arrow-BOVO1wOE.js";const f={LEADER:"Leader",CO_LEADER:"Co-leader",ADMIN:"Administrator",WEB:"Suggestions for website",SUPPORT:"You can contact them:"},C={LEADER:"Лідер",CO_LEADER:"Зам. лідера",ADMIN:"Адміністратор",WEB:"Пропозиції по сайту",SUPPORT:"Ті, до кого можна звернутися:"},P={LEADER:"Глава",CO_LEADER:"Зам. главы",ADMIN:"Администратор",WEB:"Предложения по сайту",SUPPORT:"Те, к кому можно обратиться:"},T={en:f,uk:C,ru:P},_=n=>T[n],W=()=>{const{language:n}=E(x),{LEADER:a,CO_LEADER:r,ADMIN:t,WEB:i,SUPPORT:c}=_(n),m=u.useMemo(()=>[{position:a,name:"Артем",link:"Artem_Chuvanov"},{position:r,name:"Марина",link:"MarVik3108"},{position:t,name:"Роман",link:"Matpockyx"},{position:t,name:"Санта",link:"Erbol_Santa"},{position:i,name:"Дмитрий",link:"Larin_Dmytro"}],[a,r,t,i]);return e.jsxs(L,{children:[e.jsx(R,{}),e.jsx(M,{children:c}),e.jsx(k,{component:A,children:e.jsx(g,{children:e.jsx(b,{children:m.map(({name:p,link:s,position:d})=>e.jsxs(O,{children:[e.jsx(l,{align:"center",children:d}),e.jsx(l,{align:"center",children:e.jsx("a",{href:`https://t.me/${s}`,target:"_blank",children:p})})]},s))})})})]})},L=o.div`
+  padding: 1rem 1.5rem;
+`,k=o(j)`
+  &.MuiPaper-root {
+    margin: 0 1rem;
+    width: calc(100% - 2rem);
+  }
+`,M=o.div`
+  ${h};
+  margin: 1rem;
+  display: flex;
+  align-items: center;
+`,O=o(D)`
+  &.MuiTableRow-root {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+  }
+`;export{W as default};
